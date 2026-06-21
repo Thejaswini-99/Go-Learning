65aa5157924eac8b42d02a2d7dd5272509de36a4
Thejaswini-99 <thejaswini8997@gmail.com>
2026-06-21 11:25:29 +0530

    Checking Array is Sorted or not

--- Arrays/Array_is_Sorted_Check.go
+++ Arrays/Array_is_Sorted_Check.go
@@ -1,8 +1,14 @@
-// {9, 9, 8, 7, 6}
+// {1, 2, 1, 4, 5}
 package Arrays
 
 func SortedorNot(nums []int) bool {
-	x := false
 
-	return x
+	for i := 0; i < len(nums)-1; i++ {
+		if !(nums[i] <= nums[i+1]) {
+
+			return false
+
+		}
+	}
+	return true
 }
--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	nums := []int{3, 2, 1, 5, 6, 9, 8}
-	SortedArray := []int{9, 9, 8, 7, 6}
+	SortedArray := []int{1, 2, 1, 4, 5}
 	A := Arrays.Largest(nums)
 	fmt.Println("Largest Element in the Array is", A)
 	B := Arrays.SecondLargest(nums)
 	fmt.Println("Second Largest Element in the Array is", B)
 	C := Arrays.SortedorNot(SortedArray)
-	fmt.Println("It is a Sorted Array", C)
+	fmt.Println("Sorted :", C)
 }
